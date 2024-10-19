7ab911931b1904d37470163ab8311bea174cb0c8
Yammer_king <86614150+YAMMER12@users.noreply.github.com>
2024-10-19 00:40:38 -0400

    Add files via upload

---
+++ nerp-loading/config.js
@@ -0,0 +1,41 @@
+/*
+*Author: Nepal Elite RP
+*Description: Nepal Elite Loading Screen
+*Version: 1.0
+
+
+                                        ** NRP Loading Screen Install **
+                                1- Watch this first = https://streamable.com/nsv7dx
+                                **{If you don't have a github account https://github.com/signup?source=login}
+
+                                2- When you finish the video = open steamkey.lua and paste your steamkey "here"
+                                3 - The is finished, thanks for your support
+
+*/
+
+
+
+UMLoadingScreen = []; // 
+
+UMLoadingScreen.GithubUserName = "";
+UMLoadingScreen.GithubRawUrl = "https://raw.githubusercontent.com/"+UMLoadingScreen.GithubUserName+"/um-loadingscreen/main/newupdates"; // Server Updates
+
+
+// UM-General Settings //
+UMLoadingScreen.ServerName = "Nepal Elite Roleplay";
+UMLoadingScreen.Logo = "logo.png";  //  ui/assets/img
+UMLoadingScreen.BackgroundImage = "bg1.png";  // ui/assets/img/bg
+UMLoadingScreen.TemplateColorHex = "#e92a3b";  // https://www.color-hex.com
+UMLoadingScreen.Music = "audio.mp3"; // ui/assets/audio/
+UMLoadingScreen.MusicVolume = 0.2;
+
+// UM-Extra Settings //
+UMLoadingScreen.AutoSnow = false; // true auto snow
+UMLoadingScreen.RandomBackground = false; // true RandomBackground
+UMLoadingScreen.RandomBackgroundImg = ['bg1.png','bg2.png']; 
+
+// UM-Lang Settings //
+UMLoadingScreen.Text1 = "Welcome,";
+UMLoadingScreen.Text2 = "New Updates";
+UMLoadingScreen.Text3 = "© 2024 NERP | All rights reserved.";
+
